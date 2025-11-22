e69a2ccc9eb4a1c324a1b88e1978d0b3051714ab
emmadorably <79769966+emmadorably@users.noreply.github.com>
2025-11-22 11:31:38 -0800

    Update wagmi.config.js

--- frontend/src/wagmi.config.js
+++ frontend/src/wagmi.config.js
@@ -1,6 +1,7 @@
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
 import { sepolia } from 'wagmi/chains';
 import { http } from 'wagmi';
+const rpcUrl = process.env.RPC_SEPOLIA_URL || 'https://ethereum-sepolia-rpc.publicnode.com'; // Fallback for local
 
 // Simple default configuration that includes all popular wallets automatically
 export const config = getDefaultConfig({
@@ -8,7 +9,7 @@ export const config = getDefaultConfig({
   projectId: '95be0fbf27f06934c74d670d57f44939',
   chains: [sepolia],
   transports: {
-    [sepolia.id]: http('https://rpc.sepolia.org', {
+    [sepolia.id]: http(rpcUrl, {
       batch: {
         wait: 100,
       },
