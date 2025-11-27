5fe2e236e7f9c5516b22bf1446b43381001dd7c3
emmadorably <79769966+emmadorably@users.noreply.github.com>
2025-11-27 03:23:39 +0000

    tenderly

--- frontend/src/wagmi.config.js
+++ frontend/src/wagmi.config.js
@@ -4,6 +4,25 @@ import { http } from 'wagmi';
 
 const sepoliaRpcUrl = process.env.RPC_SEPOLIA_URL || 'https://ethereum-sepolia-rpc.publicnode.com';
 const hoodiRpcUrl = process.env.RPC_HOODI_URL || 'https://ethereum-hoodi-rpc.publicnode.com';
+const tenderlyRpcUrl = 'https://virtual.mainnet.us-west.rpc.tenderly.co/47607c89-e50a-4805-a15c-7d2c55d351f3';
+
+// Define Tenderly virtual testnet
+const tenderly = {
+  id: 1,
+  name: 'Tenderly',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'Ether',
+    symbol: 'ETH',
+  },
+  rpcUrls: {
+    default: { http: [tenderlyRpcUrl] },
+  },
+  blockExplorers: {
+    default: { name: 'Tenderly Explorer', url: 'https://dashboard.tenderly.co/explorer/vnet/6e9729a9-2365-49cd-aaa0-4a07f31753d2/transactions' },
+  },
+  testnet: true,
+};
 
 // Define Hoodi testnet chain (not yet in wagmi/chains)
 const hoodi = {
@@ -27,8 +46,15 @@ const hoodi = {
 export const config = getDefaultConfig({
   appName: 'Rose Token',
   projectId: '95be0fbf27f06934c74d670d57f44939',
-  chains: [hoodi, sepolia],
+  chains: [tenderly, hoodi, sepolia],
   transports: {
+    [tenderly.id]: http(tenderlyRpcUrl, {
+      batch: {
+        wait: 100,
+      },
+      retryCount: 3,
+      timeout: 30_000,
+    }),
     [hoodi.id]: http(hoodiRpcUrl, {
       batch: {
         wait: 100,
